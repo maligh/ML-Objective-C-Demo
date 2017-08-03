07c482c6e2dc86f033c50c0e1bcb07c6fe027914
mali <455080311@qq.com>
2017-08-03 17:37:04 +0800

    Description idea

---
+++ MLDescription/NSObject+MLDescription.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+MLDescription.h
+//  TestWei
+//
+//  Created by MaLi on 2017/7/27.
+//  Copyright © 2017年 mali. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (MLDescription)
+
+@end
